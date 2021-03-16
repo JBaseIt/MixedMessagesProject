b7e82763291acbf5a452094399a2456f225820c5
Nick <66110985+JBaseIt@users.noreply.github.com>
2021-03-15 17:43:19 -0700

    More tests on changing element content

--- script.js
+++ script.js
@@ -10,13 +10,13 @@ let randAdv = adverbs[Math.floor(Math.random() * adverbs.length)];
 let messages = [
     `This is a random sentence. It will contain a random adjective (${randAdj}), a random noun (${randNoun}), a random verb (${randVerb}), and maybe even a random adverb (${randAdv}).`,
     `This is another test sentence. It has all the random components as well: ${randAdj}, ${randNoun}, ${randVerb}, ${randAdv}.`
-];
+]; 
 
 let randMess = messages[Math.floor(Math.random() * messages.length)];
 console.log(randMess);
 
-function greetings () { //Testing grabbing user input and displaying on action
+function paragraphChange () { //Testing grabbing user input and displaying on action
 let testing = document.getElementById("userinput").value;
 
-document.write("Hello!!, " + testing);
+document.getElementById("p1").innerHTML(`You have typed: ${testing}`);
 };
